55f46a47886a7199e36022c07135e3c36bca4202
anyakudojunior <anyakudojunior@gmail.com>
2026-05-14 15:43:10 +0200

    Update Interactive_dashboard.py

--- Dashboard/Interactive_dashboard.py
+++ Dashboard/Interactive_dashboard.py
@@ -13,9 +13,10 @@ model = lgb.Booster(model_file=os.path.join(directory, "cycling_model.txt"))
 
 print(model.pandas_categorical)
 pandas_cat = model.pandas_categorical
-st.write(os.listdir(directory))
+sites = pd.read_csv(os.path.join(directory, "sites.csv"), header=None)
+st.write(sites.shape)
+st.write(sites.head())
 st.stop()
-sites = pd.read_csv(os.path.join(directory, "sites.csv"), sep=",", header=None, skipinitialspace=True)
 sites.columns = [
     "index", "siteID", "long_site", "lat_site", "name", "operator",
     "road", "district", "municipality", "interval", "date"
